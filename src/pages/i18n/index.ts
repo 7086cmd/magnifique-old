bf6ccf27ad9f5166cfc922356a51854ed3dc8683
Wu Chengyu <7086cmd@gmail.com>
2022-01-15 22:57:54 +0800

    bug fix

--- src/pages/i18n/index.ts
+++ src/pages/i18n/index.ts
@@ -1,12 +1,9 @@
-import en from './locales/en-us'
 import zh from './locales/zh-cn'
 import { createI18n } from 'vue-i18n'
 const i18n = createI18n({
-    locale: navigator.languages[0],
+    locale: 'zh-CN',
     messages: {
         'zh-CN': zh,
-        'en-US': en,
-        en,
     },
     legacy: false,
 })
